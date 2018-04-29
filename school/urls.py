e02d86eb73efba2dee97f6c509109c68675909bc
eyadkobatte <ekobatte96@gmail.com>
2018-04-29 16:32:00 +0530

    Pushing to heroku

--- school/urls.py
+++ school/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index)
+    url(r'^$', views.index),
+    url(r'^standards$', views.standards),
+    url(r'^categories$', views.categories)
 ]
--- school/views.py
+++ school/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Standard, Category
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def standards(request):
+    standard = Standard.objects.all().values_list('class_name',flat=True)
+    context = {
+        'standard': standard
+    }
+    return render(request, 'standards.html', context)
+
+def categories(request):
+    category = Category.objects.all().values_list('category_name', flat=True)
+    context = {
+        'category': category
+    }
+    return render(request, 'categories.html', context)
